5df62ed1338fc5734d5a732814665988dd464230
Andruid929 <princeajsa@gmail.com>
2025-04-14 09:49:14 +0200

    Added ReadFile to allow having an object with file name and data

---
+++ src/main/java/net/druidlabs/ajse/ReadFile.java
@@ -0,0 +1,97 @@
+package net.druidlabs.ajse;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Class that converts a file into an object containing the file's name, file extension, and file contents.
+ * Objects can only be created from calling the static {@link #getFile(String, String)}
+ *
+ * @version 1.0
+ * @since 1.0
+ * */
+
+public class ReadFile extends TextFileOperation {
+
+    private final String fileName;
+    private final String contents;
+    private final String filePath;
+
+    private ReadFile(String filePath, String fileName) throws IOException {
+        File thisFile = getFile(filePath, fileName);
+
+        if (!thisFile.exists()) {
+            throw new IOException("File doesn't exist");
+        } else if (!thisFile.isFile()) {
+            throw new IOException("Found folder and not file");
+        }
+
+        this.fileName = fileName;
+        this.filePath = filePath;
+
+        contents = TextFileReader.readThisFile(filePath, fileName);
+    }
+
+    /**
+     * Get the contents of the file read.
+     *
+     * @since 1.0
+     * @return {@code String} of the file contents
+     * */
+
+    public String getContents() {
+        return contents;
+    }
+
+    /**
+     * Get the name of the file read.
+     *
+     * @since 1.0
+     * @return {@code String} of the file name
+     * */
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * Get the path of the file read.
+     *
+     * @since 1.0
+     * @return {@code String} of the file's location
+     * */
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ReadFile readFile = (ReadFile) o;
+        return Objects.equals(fileName, readFile.fileName) && Objects.equals(contents, readFile.contents) && Objects.equals(filePath, readFile.filePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fileName, contents, filePath);
+    }
+
+    /**
+     * Create an instance of this class.
+     *
+     * @return {@code ReadFile} object containing the data of the file passed in.
+     * @since 1.0
+     * */
+
+    @Contract(pure = true)
+    @NotNull
+    public static ReadFile getThisFile(String filePath, String fileName) throws IOException {
+        return new ReadFile(filePath, fileName);
+    }
+
+}
