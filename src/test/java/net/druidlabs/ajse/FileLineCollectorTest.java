1c007341041635899affe845a07a1fffd97f96b7
Andruid929 <princeajsa@gmail.com>
2025-05-08 10:09:06 +0200

    Added tests for FileLineCollector

--- src/test/java/net/druidlabs/ajse/FileLineCollectorTest.java
+++ src/test/java/net/druidlabs/ajse/FileLineCollectorTest.java
@@ -3,19 +3,28 @@ package net.druidlabs.ajse;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileLineCollectorTest {
 
+    private final String testFileName = "TestFile3.txt";
+
     @Test
     void collectUnsortedLines() throws IOException {
-        FileLineCollector.collectUnsortedLines("", "TestFile3.txt");
+        Set<String> lines = FileLineCollector.collectUnsortedLines("", testFileName);
+
+        assertTrue(lines.contains("Will be collected"));
     }
 
     @Test
-    void collectSortedLines() {
+    void collectSortedLines() throws IOException {
+        List<String> lines = FileLineCollector.collectSortedLines("", testFileName);
+
+        assertEquals("Seeing if the lines", lines.get(1));
     }
 
-    @Test
-    void collectLines() {
-    }
 }
